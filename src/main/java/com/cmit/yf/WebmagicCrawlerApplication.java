b965091ae978b9d1e61a205a95c8698ab616ccbe
yangfanonline <392272124@qq.com>
2018-06-10 17:06:38 +0800

    OG

---
+++ src/main/java/com/cmit/yf/WebmagicCrawlerApplication.java
@@ -0,0 +1,12 @@
+package com.cmit.yf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmagicCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmagicCrawlerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cmit/yf/WebmagicCrawlerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cmit.yf;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebmagicCrawlerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
